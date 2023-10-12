30e7f6078558487adcac16cf30abb2e0dd9815f3
Oumadenis1779 <dennies.ouma@student.moringaschool.com>
2023-10-12 18:17:12 +0300

    completed.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let films=[]
+let films=[];
 function fetchData() {
     return fetch("http://localhost:3000/films")
     .then(res=>res.json())
@@ -12,10 +12,10 @@ let movieTitle=document.getElementById("title")
 let moviePoster=document.getElementById("movie-poster");
 let movieRuntime=document.getElementById("runTime");
 let movieShowtime=document.getElementById("showTime");
-let availableTickets=document.getElementById("available tickets")
-let movieButton=document.getElementById("btn")
-let movieCapacity=document.getElementById("capacity")
-let movieSoldTichet=document.getElementById("sold")
+let availableTickets=document.getElementById("available tickets");
+let movieButton=document.getElementById("btn");
+let movieCapacity=document.getElementById("capacity");
+let movieSoldTichet=document.getElementById("sold");
 films.forEach((film)=> {
  movieTitle.innerText=film.title;
  moviePoster.src =film.poster;
@@ -23,16 +23,16 @@ movieRuntime.innerText=film.runtime;
 movieShowtime.innerText=film.showtime;
 
 movieButton.addEventListener("click",function(e){
-    e.preventdefult
-    let y=availableTickets.innerText=film.capacity-film.tickets_sold
+    `e.preventdefult()`
+    let y = availableTickets.innerText=film.capacity-film.tickets_sold
    
     if (y > 0) {
-     alert(`Ticket purchased successfully ${y-1} tickets are still available for booking`) ;
+     alert(`Ticket purchased successfully ${y-1} tickets are still available for booking.`) ;
   } else {
     alert('Sorry, this showing is sold out.');
   }
 
-   }) 
+   });
 })
    
 }
